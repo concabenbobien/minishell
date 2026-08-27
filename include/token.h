770209d1fc26930a22a4d86d0216446a51b56cf8
concabenbobien <khanhgia2972005@gmail.com>
2026-08-28 02:02:02 +0700

    feat: define lexer token representation

---
+++ include/token.h
@@ -0,0 +1,30 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <stddef.h>
+typedef enum {
+    TOK_WORD,
+    TOK_DIRECT_APPEND,
+    TOK_DIRECT_OUT,
+    TOK_DIRECT_IN,
+    TOK_EOF
+} 
+TokenType;
+
+typedef struct 
+{
+    TokenType type;
+    char *lexeme;
+    size_t start;   /* Starting position in the source line. */
+    size_t length;  /* Length of the token in the source line. 
+                    Length after normalized is strlen(token.lexeme)*/
+} Token;
+
+typedef struct 
+{
+    Token *items;
+    size_t capacity;
+    size_t count;
+} TokenStream;
+
+#endif
